e629ac894295f3a1a0cdabb459445d3f07a6491f
Sowmya, HL <Sowmya.HL@fmr.com>
2026-06-05 20:53:42 +0530

    Revert "fix github actions workflow"
    This reverts commit cea0339e1f623ddc55df1e7aad29f4d1d707faa0.

--- libs/grid/src/test-setup.ts
+++ libs/grid/src/test-setup.ts
@@ -5,13 +5,9 @@ import '@angular/localize/init';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
 // Polyfill provided by https://github.com/nrwl/nx/issues/1178#issuecomment-498924910
-if (!('DragEvent' in window)) {
-  Object.defineProperty(window, 'DragEvent', {
-    configurable: true,
-    writable: true,
-    value: class DragEvent extends Event {},
-  });
-}
+Object.defineProperty(window, 'DragEvent', {
+  value: class DragEvent {},
+});
 
 getTestBed().resetTestEnvironment();
 getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting(), {
