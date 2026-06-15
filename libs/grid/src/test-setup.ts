23a0ae327a1b692b86ec1c6c549cc0080071f9da
Scharp, Michael <michael.scharp@fmr.com>
2026-06-15 14:34:17 -0600

    Remove manual drag even mocking
    Signed-off-by: Scharp, Michael <michael.scharp@fmr.com>

--- libs/grid/src/test-setup.ts
+++ libs/grid/src/test-setup.ts
@@ -4,11 +4,6 @@ import { getTestBed } from '@angular/core/testing';
 import '@angular/localize/init';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
-// Polyfill provided by https://github.com/nrwl/nx/issues/1178#issuecomment-498924910
-Object.defineProperty(window, 'DragEvent', {
-  value: class DragEvent {},
-});
-
 getTestBed().resetTestEnvironment();
 getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting(), {
   teardown: { destroyAfterEach: false },
