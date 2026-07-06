ceb911c750ef0ef3df2050bc96a7e4ce7f4ffd38
Terry, Morgan <morgan.terry@fmr.com>
2026-07-06 09:41:03 -0600

    Reset testing module in global afterEach

--- libs/common/src/test-setup.ts
+++ libs/common/src/test-setup.ts
@@ -3,6 +3,7 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import '@angular/localize/init';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import { afterEach } from 'vitest';
 
 if (typeof window.DragEvent === 'undefined') {
   Object.defineProperty(window, 'DragEvent', {
@@ -13,4 +14,8 @@ if (typeof window.DragEvent === 'undefined') {
 getTestBed().resetTestEnvironment();
 getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting(), {
   teardown: { destroyAfterEach: false },
+});
+
+afterEach(() => {
+  getTestBed().resetTestingModule();
 });
